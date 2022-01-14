29be97582a190d5679f6c0181d4546f6ffd18a4d
Joseph Galvin <jwgalvin@gmail.com>
2022-01-13 19:59:13 -0700

    Setup test for character map

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,6 +10,12 @@ describe do Enigma
     enigma = Enigma.new
     enigma.encrypt("hello world", "02715", "040895")
     expect(enigma).to be_a(Enigma)
-    expect(enigma.key_grabber("02715")).to eq(["0","2","7","1","5"])
+    expect(enigma.key_grabber("02715")).to eq(["02","27","71", "15"])
+    expect(enigma.offset_grabber("040895")).to eq([1, 0, 2, 5])
+  end
+
+  it 'will test the character map' do
+    enigma = Enigma.new
+    expect(enigma.char_map).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
 end
