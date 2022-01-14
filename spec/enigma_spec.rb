fbe1029f2064258e548f43b02cd1e91da7ef32e7
Joseph Galvin <jwgalvin@gmail.com>
2022-01-13 20:39:12 -0700

    add test for random date.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,16 +7,27 @@ require './lib/key_shifter'
 
 
 describe do Enigma
-  it 'exists and takes arguments' do
+  it 'exists' do
     enigma = Enigma.new
     enigma.encrypt("hello world", "02715", "040895")
     expect(enigma).to be_a(Enigma)
-    expect(enigma.key_grabber("02715")).to eq(["02","27","71", "15"])
-    expect(enigma.offset_grabber("040895")).to eq([1, 0, 2, 5])
   end
 
   it 'will test the character map' do
     enigma = Enigma.new
     expect(enigma.characters).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it 'will test keys here, manual input now' do
+    enigma = Enigma.new
+    expect(enigma.key_grabber("02715")).to eq(["02","27","71", "15"])
+    # expect(enigma.shift)
+  end
+
+  it 'will test offset here, manual input now' do
+    enigma = Enigma.new
+    expect(enigma.offset_grabber("040895")).to eq([1, 0, 2, 5])
+    #this will change every day!
+    expect(enigma.offset_grabber).to be("13  1  22")
   end
 end
